b9eda1055f36df8086599c1dc381f4b54cbdec4e
dante0527 <19dante.j.lee@gmail.com>
2022-04-02 00:30:50 -0400

    whitespace edits

--- one_player.py
+++ one_player.py
@@ -4,17 +4,16 @@ import random
 username = input("\nEnter your name:\n")
 
 # Print welcome message using user's name
-print(f"\nWelcome to the higher/lower game, {username}!")
+print(f"\nWelcome to the higher/0 game, {username}!")
 
-# Accept upper and lower bounds as inputs
-lower = int(input("Enter the lower bound: "))
-upper = int(input("Enter the upper bound: "))
+# Accept number range as input
+num_range = int(input("Enter the number range: "))
 
-# Create random number between upper and lower bounds
-secret_num = random.randint(lower, upper)
+# Create random number between 0 and num_range
+secret_num = random.randint(0, num_range)
 
 # Prompt user to guess number within bounds
-print(f"\nGreat, now guess a number between {lower} and {upper}")
+print(f"\nGreat, now guess a number between 0 and {num_range}")
 
 # Accept first guess
 guess = int(input())
@@ -22,22 +21,22 @@ tries = 0
 
 # Initiate while loop
 while guess != secret_num:
-    if lower > guess or guess > upper:
+    if 0 > guess or guess > num_range:
         print("Guess outside of range.\n"
-              f"Guess a number between {lower} and {upper}.")
+              f"Guess a number between 0 and {num_range}.")
         guess = int(input())
     elif guess < secret_num:
+        tries += 1
         print("Nope, too low.")
         guess = int(input())
-        tries += 1
     elif guess > secret_num:
+        tries += 1
         print("Nope, too high.")
         guess = int(input())
-        tries += 1
         
 # Announce victory once while loop is exited
 tries += 1
 if tries == 1:
-    print(f"You got it in 1 try!\nThe number was {secret_num}.")
+    print(f"\nYou got it in 1 try!\nThe number was {secret_num}.")
 else:
-    print(f"You got it in {tries} tries!\nThe number was {secret_num}.")
+    print(f"\nYou got it in {tries} tries!\nThe number was {secret_num}.")
