47bc53fc225be504303d7806d4b70979cafa22bc
dante0527 <19dante.j.lee@gmail.com>
2022-04-02 00:40:16 -0400

    changed num range selection for two player mode

--- two_player.py
+++ two_player.py
@@ -4,11 +4,10 @@ import random
 # Guess function
 def guess_num(user):
     global winner
-    user_guess = int(input(f"\n{user}'s turn.\n"
-                           f"Guess a number between {lower} and {upper}:\n"))
-    if lower > user_guess or user_guess > upper:
+    user_guess = int(input(f"\n{user}'s turn.\n"))
+    if 0 > user_guess or user_guess > num_range:
         print("Guess outside of range.\n"
-              f"Guess must be between {lower} and {upper}.")
+              f"Guess must be between 0 and {num_range}.")
     elif user_guess < secret_num:
         print("Nope, too low.")
     elif user_guess > secret_num:
@@ -25,12 +24,11 @@ player2 = input("Enter second player's name:\n")
 # Print welcome message using user's name
 print(f"\nWelcome to the higher/lower game, {player1} and {player2}!")
 
-# Accept upper and lower bounds as inputs
-lower = int(input("Enter the lower bound: "))
-upper = int(input("Enter the upper bound: "))
+# Accept num_range as input
+num_range = int(input("Enter the num_range bound: "))
 
-# Create random number between upper and lower bounds
-secret_num = random.randint(lower, upper)
+# Create random number between 0 and num_range
+secret_num = random.randint(0, num_range)
 
 # Track tries and winner
 tries = 0
@@ -38,6 +36,8 @@ winner = ''
 
 # Initiate while loop
 while winner == '':
+    if tries == 0:
+        print(f"Guess a number between 0 and {num_range}:\n")
     tries += 1
     if winner == '':
         guess_num(player1)
